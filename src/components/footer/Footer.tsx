c2363bd8f79bd62c50b8ebb2e9bc0be48b83199a
Yo Han Joo <yohanjoo98@gmail.com>
2024-08-28 12:42:34 +0900

    add call to action (#9)
    * add call to action
    
    * fix build error
    
    * Create footer
    
    * refactor a bit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className="w-full h-[180px] pt-[100px] pb-10 flex-col justify-start items-center gap-4 inline-flex">
+      <div className="w-full max-w-[1224px] h-[0px] border border-[#282d45]"></div>
+      <div className="w-full maw-w-[1224px] justify-between items-center inline-flex">
+        <div className="w-full text-[#8f9bb7] text-base font-normal text-center font-['Inter'] leading-normal">
+          Copyright 2024 © PRIZM
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Main from "../components/main";
+import CallToAction from "../components/call-to-action";
 import Navigation from "../components/navigation";
+import Footer from "../components/footer";
 
 export default function Home(): JSX.Element {
   return (
     <Main>
       <Navigation />
+      <CallToAction />
+      <Footer />
     </Main>
   );
 }
