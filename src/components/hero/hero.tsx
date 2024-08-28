72880bcf0a55c62c45314958afa79fda37a579cf
John Wesley Baik <johnwbaik@gmail.com>
2024-08-28 12:54:29 +0900

    Created structure for hero

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+	return (
+		<div className='w-[full] flex justify-center items-center'>
+			<div className='flex flex-col items-center max-w-[77rem]'>
+				<h1 className='text-[4rem] font-inter font-bold text-white'>
+					An End-to-End IoT Solution for Everyone.
+				</h1>
+				<div className='flex justify-center'>
+					<span className='text-white'>
+						PRIZM is an end-to-end IoT solution that provides Out-of-the-Box
+						Hardware, Connectivity, Device Software, and Cloud Services to
+						streamline IoT product development, requiring no technical expertise
+						while ensuring Best-in-Class Security. With PRIZM, you can launch
+						your IoT product and service in no time at the lowest possible cost,
+						leading to your IoT journey to success.
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import Navigation from "../components/navigation";
+import Navigation from '../components/navigation';
+import Hero from '../components/hero/hero';
 
 export default function Home(): JSX.Element {
-  return (
-    <div className="min-h-screen bg-[#151934] px-4">
-      <Navigation />
-    </div>
-  );
+	return (
+		<div className='min-h-screen bg-[#151934] px-4'>
+			<Navigation />
+			<Hero />
+		</div>
+	);
 }
