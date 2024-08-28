015d968c787e563058cd9ac4a02f3025ac2e8b69
Yo Han Joo <yohanjoo98@gmail.com>
2024-08-28 12:32:07 +0900

    refactor & main

--- src/components/navigation/hamburger-button-icon.tsx
+++ src/components/navigation/hamburger-button-icon.tsx
@@ -6,7 +6,7 @@ export default function HamburgerButtonIcon() {
       height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
