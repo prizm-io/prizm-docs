4840b5d8977846482256104553b13995f14261cf
Yo Han Joo <yohanjoo98@gmail.com>
2024-08-28 11:30:50 +0900

    Create footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import CallToAction from "../components/cta/CallToAction";
 import Navigation from "../components/navigation";
+import Footer from "../components/footer/Footer";
 
 export default function Home(): JSX.Element {
   return (
     <div className="min-h-screen bg-[#151934] px-4">
       <Navigation />
       <CallToAction />
+      <Footer />
     </div>
   );
 }
