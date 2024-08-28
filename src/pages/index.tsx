0ec5c1a31ff96e5658d98ef9020a0228b43ad7cc
Yo Han Joo <yohanjoo98@gmail.com>
2024-08-28 13:32:00 +0900

    Add Benefits (#10)
    * add `Benefits`
    
    * to camelCase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import Main from "../components/main";
 import CallToAction from "../components/call-to-action";
 import Navigation from "../components/navigation";
 import Footer from "../components/footer";
+import Benefits from "../components/benefits";
 
 export default function Home(): JSX.Element {
   return (
     <Main>
       <Navigation />
+      <Benefits />
       <CallToAction />
       <Footer />
     </Main>
