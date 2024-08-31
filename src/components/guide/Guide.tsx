6c57cd0ad1f635b3a7a7fe715248b9a0c9c411c3
John Wesley Baik <johnwbaik@gmail.com>
2024-08-31 23:19:43 +0900

    Refactored code

--- src/components/guide/Guide.tsx
+++ src/components/guide/Guide.tsx
@@ -51,91 +51,80 @@ export default function Guide() {
   };
 
   return (
-    <>
-      <div className="py-[70px]">
-        <GuideTitle />
-        <div ref={ref} className="flex justify-center items-center">
-          <div className="max-w-[1224px] w-full justify-center flex flex-row">
-            <Tabs
-              value={selectedTab}
-              onValueChange={handleTabChange}
-              className="text-[#8f9bb7] flex flex-col lg:px-3 items-center w-[1224px] text-base font-normal"
-            >
-              <TabsList className="flex flex-col xl:flex-row w-full justify-between gap-6">
-                <Tab onTabClick={resetInterval} value="connect">
-                  <TabIcon
-                    icon={PieIcon}
-                    isActive={selectedTab === "connect"}
-                  />
-                  1. Connect Sensors
-                </Tab>
-                <Tab onTabClick={resetInterval} value="build">
-                  <TabIcon
-                    icon={BuildIcon}
-                    isActive={selectedTab === "build"}
-                  />
-                  2. Build the App
-                </Tab>
-                <Tab onTabClick={resetInterval} value="deploy">
-                  <TabIcon
-                    icon={DeployIcon}
-                    isActive={selectedTab === "deploy"}
-                  />
-                  3. Deploy the App
-                </Tab>
-                <Tab onTabClick={resetInterval} value="scale">
-                  <TabIcon
-                    icon={ScaleIcon}
-                    isActive={selectedTab === "scale"}
-                  />
-                  4. Scale to Millions
-                </Tab>
-              </TabsList>
-              <div className="text-lg h-full xl:h-w-full justify-center pt-[60px]">
-                <TabInfo value="connect">
-                  <TabInfoImg src="img/working.png" />
-                  <TabDescription>
-                    Simply connect your sensors to PRIZM's out-of-box hardware.
-                    Whether they are temperature sensors, motion sensors, or any
-                    other hardware that needs to communicate, PRIZM takes care
-                    of the rest and ensures secure and reliable connectivity.
-                  </TabDescription>
-                </TabInfo>
-                <TabInfo value="build">
-                  <TabInfoImg src="img/working.png" />
-                  <TabDescription>
-                    Build your dream application effortlessly with the advanced
-                    app builder, featuring drag-and-drop functionality to
-                    connect sensors / actuators / other systems, implement
-                    business logic, and seamlessly send data to the cloud or
-                    trigger notifications for warnings and errors.
-                  </TabDescription>
-                </TabInfo>
-                <TabInfo value="deploy">
-                  <TabInfoImg src="img/working.png" />
-                  <TabDescription>
-                    Deploy your application seamlessly from the web platform.
-                    Whether you’re launching a small pilot or a large-scale
-                    rollout, PRIZM ensures your app is deployed safely and
-                    securely. Get your solution up and running quickly, with
-                    minimal downtime and maximum reliability.
-                  </TabDescription>
-                </TabInfo>
-                <TabInfo value="scale">
-                  <TabInfoImg src="img/working.png" />
-                  <TabDescription>
-                    Bring your MVP to market quickly and test your business
-                    model in real-world scenarios, giving you the confidence to
-                    expand your business. PRIZM helps you capitalize on market
-                    opportunities faster, ensuring you stay ahead of the
-                    competition.
-                  </TabDescription>
-                </TabInfo>
-              </div>
-            </Tabs>
-          </div>
+    <div className="py-[70px]">
+      <GuideTitle />
+      <div ref={ref} className="flex justify-center items-center">
+        <div className="max-w-[1224px] w-full justify-center flex flex-row">
+          <Tabs
+            value={selectedTab}
+            onValueChange={handleTabChange}
+            className="text-[#8f9bb7] flex flex-col lg:px-3 items-center w-[1224px] text-base font-normal"
+          >
+            <TabsList className="flex flex-col xl:flex-row w-full justify-between gap-6">
+              <Tab onTabClick={resetInterval} value="connect">
+                <TabIcon icon={PieIcon} isActive={selectedTab === "connect"} />
+                1. Connect Sensors
+              </Tab>
+              <Tab onTabClick={resetInterval} value="build">
+                <TabIcon icon={BuildIcon} isActive={selectedTab === "build"} />
+                2. Build the App
+              </Tab>
+              <Tab onTabClick={resetInterval} value="deploy">
+                <TabIcon
+                  icon={DeployIcon}
+                  isActive={selectedTab === "deploy"}
+                />
+                3. Deploy the App
+              </Tab>
+              <Tab onTabClick={resetInterval} value="scale">
+                <TabIcon icon={ScaleIcon} isActive={selectedTab === "scale"} />
+                4. Scale to Millions
+              </Tab>
+            </TabsList>
+            <div className="text-lg h-full xl:h-w-full justify-center pt-[60px]">
+              <TabInfo value="connect">
+                <TabInfoImg src="img/working.png" />
+                <TabDescription>
+                  Simply connect your sensors to PRIZM's out-of-box hardware.
+                  Whether they are temperature sensors, motion sensors, or any
+                  other hardware that needs to communicate, PRIZM takes care of
+                  the rest and ensures secure and reliable connectivity.
+                </TabDescription>
+              </TabInfo>
+              <TabInfo value="build">
+                <TabInfoImg src="img/working.png" />
+                <TabDescription>
+                  Build your dream application effortlessly with the advanced
+                  app builder, featuring drag-and-drop functionality to connect
+                  sensors / actuators / other systems, implement business logic,
+                  and seamlessly send data to the cloud or trigger notifications
+                  for warnings and errors.
+                </TabDescription>
+              </TabInfo>
+              <TabInfo value="deploy">
+                <TabInfoImg src="img/working.png" />
+                <TabDescription>
+                  Deploy your application seamlessly from the web platform.
+                  Whether you’re launching a small pilot or a large-scale
+                  rollout, PRIZM ensures your app is deployed safely and
+                  securely. Get your solution up and running quickly, with
+                  minimal downtime and maximum reliability.
+                </TabDescription>
+              </TabInfo>
+              <TabInfo value="scale">
+                <TabInfoImg src="img/working.png" />
+                <TabDescription>
+                  Bring your MVP to market quickly and test your business model
+                  in real-world scenarios, giving you the confidence to expand
+                  your business. PRIZM helps you capitalize on market
+                  opportunities faster, ensuring you stay ahead of the
+                  competition.
+                </TabDescription>
+              </TabInfo>
+            </div>
+          </Tabs>
         </div>
       </div>
-    </>
+    </div>
   );
 }
